702c6aa6faa67803fbdc6a6bfc567e19653eb633
BboyAkers <austin.akers5@gmail.com>
2023-02-26 13:27:59 -0800

    added assets/icons

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="container">
-    <h1>This is a home page</h1>
-  </div>
+  <main>
+    <section>
+      <div class="fixed top-0 left-0 z-50 w-full h-16 p-7 bg-blue-dark">
+        <img src="../assets/logo.svg" alt="" />
+      </div>
+    </section>
+    <section>
+      <div class="md:container md:mx-auto md:px-10">
+        <h1>This is a home page</h1>
+      </div>
+    </section>
+  </main>
 </template>
