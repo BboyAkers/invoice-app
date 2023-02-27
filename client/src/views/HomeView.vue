2d3f5bd1522d222ad46c6caf6db6d17335ee0ca4
Austin Akers <austin.akers5@gmail.com>
2023-02-26 17:07:55 -0800

    finished UI for mobile nav bar

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,23 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MobileNav from '@/components/navbar/MobileNav.vue'
+</script>
 
 <template>
   <main>
     <nav>
-      <div
-        class="fixed top-0 left-0 z-50 inline-flex items-center justify-between w-full h-16 pr-2 bg-blue-dark"
-      >
-        <div class="inline-flex items-center w-16 h-full rounded-r-xl bg-purple-light">
-          <img class="mx-auto" src="@/assets/logo.svg" alt="" />
-        </div>
-        <div class="inline-flex h-full gap-5 rounded-r-xl">
-          <img class="self-center h-6" src="@/assets/icon-moon.svg" alt="" />
-          <div class="border-l border-solid border-purple-extradark"></div>
-          <img class="self-center h-10 rounded-full" src="@/assets/image-avatar.jpg" alt="" />
-        </div>
-      </div>
+      <MobileNav />
     </nav>
     <section>
-      <div class="md:container md:mx-auto md:px-10">
+      <div class="mt-20 md:container md:mx-auto md:px-10">
         <h1>This is a home page</h1>
       </div>
     </section>
