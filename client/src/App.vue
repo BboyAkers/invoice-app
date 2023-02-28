6e4e9651cd63284762c62ee3d66d188275a3b745
BboyAkers <austin.akers5@gmail.com>
2023-02-27 22:24:27 -0800

    added go back button and added svg component support

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,7 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header> -->
-
   <RouterView />
 </template>
